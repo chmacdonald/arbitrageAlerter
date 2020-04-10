abbc8cc5103be2186bd3d0cd3632f0d77bbd17c5
Chris M <chrismacdonald@wustl.edu>
2020-04-10 16:25:20 -0400

    Updated predictScrapper

--- predictIT.py
+++ predictIT.py
@@ -1,14 +1,18 @@
-import requests
-from bs4 import BeautifulSoup
+# import requests
+# import csv
+# import xml.etree.ElementTree as ET
+# from bs4 import BeautifulSoup
+# URL = 'https://www.predictit.org/api/marketdata/markets/4292/'
+# xml = requests.get(URL)
+# page = ET.parse(xml)
+# # soup = BeautifulSoup(page.content, 'XML.parser')
+# # results = soup.find(id='4292')
+# print(page.text)
+import urllib2
+import xmltodict
+file = urllib2.urlopen('https://www.predictit.org/api/marketdata/markets/4292/')
+data = file.read()
+file.close()
 
-URL = 'https://www.predictit.org/api/marketdata/markets/4292/'
-
-page = requests.get(URL)
-
-soup = BeautifulSoup(page.content, 'html.parser')
-
-results = soup.find(id='4292')
-
-
-print(page.text)
-
+data = xmltodict.parse(data)
+print(render_to_response('my_template.html', {'data': data}))
